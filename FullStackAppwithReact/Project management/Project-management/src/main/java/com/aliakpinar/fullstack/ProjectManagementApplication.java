abf607301cce42f5858162399f1f5593048a5b55
AliAkpnar <aliakpinar3435@gmail.com>
2020-10-18 00:51:18 +0300

    FC

---
+++ FullStackAppwithReact/Project management/Project-management/src/main/java/com/aliakpinar/fullstack/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.aliakpinar.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+	}
+
+}
